50385f52e90374761cc3c3c9e5390d9467cfbf54
Markus Strasser <strasser.ms@gmail.com>
2018-11-29 10:15:33 +0100

    iex getter xml req working, fetch was buggy

--- src/App.js
+++ src/App.js
@@ -8,6 +8,31 @@ import Plot from 'react-plotly.js';
 //TODO flexbox wrap multiple plots
 
 
+const stockAPI = 'https://api.iextrading.com/1.0/';
+const test1 = 'stock/aapl/chart/1d';
+
+
+//because fetch API wasn't friendly with iex api ...
+function simpleGet(theUrl, noFail=true) {
+    return new Promise(function (resolve, reject) {
+        const xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = () => {
+            if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+                resolve(xmlHttp.responseText)
+            }
+        }
+        xmlHttp.open("GET", theUrl, true); // true for asynchronous
+        xmlHttp.send(null);
+        xmlHttp.onerror = (error) => {
+            reject(error)
+            //default, so components don't need .catch each time
+            if (noFail) throw(error)
+        }
+    })
+
+}
+
+
 export default function App() {
     const [stockData, setStockData] = useState({})
 
