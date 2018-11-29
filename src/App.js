1ece180a95ea17c9ce2b3ba0e460d3174f389128
Markus Strasser <strasser.ms@gmail.com>
2018-11-29 10:06:54 +0100

    react 16.7 setup + plotly api test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import Plot from 'react-plotly.js';
 
-class App extends Component {
-  render() {
+//TODO api calls pipeline
+//TODO seamless util mapping functions ==> consumable prop obj
+//TODO special Wrapper around <Plot/> for our obj struct
+//TODO flexbox wrap multiple plots
+
+
+export default function App() {
+    const [stockData, setStockData] = useState({})
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+            <Plot data={[
+                {
+                    x: [1, 2, 3],
+                    y: [2, 6, 3],
+                    type: 'scatter',
+                    mode: 'lines',
+                    marker: {color: 'red'},
+                },
+                {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+            ]}
+                  layout={{width: window.innerWidth * 0.8, height: window.innerHeight * 0.8, title: 'A Fancy Plot'}}
+            />
+        </div>
     );
-  }
 }
-
-export default App;
